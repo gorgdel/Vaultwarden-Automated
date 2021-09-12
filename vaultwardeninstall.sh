bd0af559ed35545eee7dc520d713a63795a43476
George <gorgdel18@gmail.com>
2021-09-12 23:53:43 +1000

    Added Nginx & Vaultwarden files

---
+++ vaultwardeninstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt-get install ranger
+apt-get install net-tools
+apt-get update
+apt-get install apt-transport-https
+apt-get install curl
+apt-get install ca-certificates
+apt-get install gnupg
+apt-get install lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=no -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+mkdir /opt/nginxproxymanager
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker pull vaultwarden/server:latest
+systemctl stop apache2
+echo "Complete - Proceed with other steps."
